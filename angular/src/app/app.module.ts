d543f06231e33153cc193ae3f47931564a6d2d75
AidaSFuentes <aidadesarrolloweb@gmail.com>
2022-01-17 16:08:20 +0100

    create calculadroa, formulario, validadores, directivas

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { environment } from 'src/environments/environment';
 import { ERROR_LEVEL, LoggerService, MyCoreModule } from 'src/lib/my-core';
+registerLocaleData(localeEs, 'es', localeEsExtra);
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,13 +14,16 @@ import { DemosComponent } from './demos/demos.component';
 import { CommonServicesModule } from './common-services';
 import { DinamicoComponent } from './dinamico/dinamico.component';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { registerLocaleData } from '@angular/common';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DemosComponent,
     DinamicoComponent,
-    CalculadoraComponent
+    CalculadoraComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule, FormsModule,
@@ -29,7 +33,16 @@ import { CalculadoraComponent } from './calculadora/calculadora.component';
   providers: [
     LoggerService,
     { provide: ERROR_LEVEL, useValue: environment.ERROR_LEVEL  },
+    { provide: LOCALE_ID, useValue: 'es-ES'},
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+function localeEs(localeEs: any, arg1: string, localeEsExtra: any) {
+  throw new Error('Function not implemented.');
+}
+
+function localeEsExtra(localeEs: (localeEs: any, arg1: string, localeEsExtra: any) => void, arg1: string, localeEsExtra: any) {
+  throw new Error('Function not implemented.');
+}
+
--- angular/src/app/calculadora/calculadora.component.ts
+++ angular/src/app/calculadora/calculadora.component.ts
@@ -1,15 +1,95 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, SimpleChanges } from '@angular/core';
+import { LoggerService } from 'src/lib/my-core';
+import { NotificationService } from '../common-services';
 
 @Component({
-  selector: 'app-calculadora',
+  selector: 'calculadora',
   templateUrl: './calculadora.component.html',
   styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent implements OnInit {
+  public readonly Math = Math;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private log: LoggerService, private notify: NotificationService) {
+    this.inicia();
   }
-
-}
+  class Calculator {
+  
+    add(num1, num2) {
+      return num1 + num2;
+    }
+  
+    subtract(num1, num2) {
+      return num1 - num2;
+    }
+  
+    division(num1, num2) {
+      return num1 / num2;
+    }
+  
+    multiply(num1, num2) {
+      return num1 * num2;
+    }
+   
+  };
+  
+  class Display {
+    init: any;
+    
+    constructor(displayFunction) {
+      this.displayFunction = displayFunction;
+      this.calculator = new Calculator();
+      this.typeOperator = undefined
+      this.currentValue = '';
+      this.previousValue = '';
+      this.signs = {
+        add: "+",
+        division: "÷",
+        multiply: "*",
+        subtract: "-",
+      }
+    }
+  
+    delete(): void{
+      this.currentValue = this.currentValue.toString().slice(0, -1);
+      this.printValues();
+    }
+  
+    deleteAll(): void {
+      this.currentValue = '';
+      this.previousValue = '';
+      this.typeOperator = undefined;
+      this.printValues();
+    }
+  
+    compute(type) {
+      this.typeOperator !== "equal" && this.calculate();
+      this.typeOperator = type;
+      this.currentValue && (this.previousValue = this.currentValue);
+      this.currentValue = '';
+      this.printValues();
+    }
+  
+    addNumber(num) {
+      if (num === '.' && this.currentValue.includes('.')) return
+      this.currentValue = this.currentValue.toString() + num.toString();
+      this.printValues();
+    }
+  
+    printValues(): void{
+      this.displayFunction(this.currentValue, `${this.previousValue} ${this.signs[this.typeOperator] || ''}`);
+    }
+  
+    calculate(): void {
+      const previousValue = parseFloat(this.previousValue);
+      const currentValue = parseFloat(this.currentValue);
+  
+      if (isNaN(currentValue) || isNaN(previousValue)) return
+      this.currentValue = this.calculator[this.typeOperator](previousValue, currentValue);
+    }
+    ngOnInit(): void {
+      if (this.init) {
+        this.compute(this.init);
+      }
+    }
+  }
