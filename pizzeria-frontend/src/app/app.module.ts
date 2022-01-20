34338a36da564948360d1f581d9842b883c539ff
AidaSFuentes <aidadesarrolloweb@gmail.com>
2022-01-20 10:04:24 +0100

    Update package json and create 3 new modules

--- pizzeria-frontend/src/app/app.module.ts
+++ pizzeria-frontend/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AccordionModule} from 'primeng/accordion';
+import {MenuItem} from 'primeng/api';
 
 @NgModule({
   declarations: [
